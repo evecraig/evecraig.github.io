0f7c82f400b962dd22ddf73e4c20d8c30fed1a31
evecraig <ecraig@hawaii.edu>
2026-05-20 15:15:20 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@1660d66fee297987403077de9c6ca91533fbe82e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Planned, ongoing, and completed projects for work or done in my free time.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
