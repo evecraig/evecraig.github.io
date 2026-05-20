394346f9abc377cf7d98c2f42313a9da1e0d01c8
evecraig <ecraig@hawaii.edu>
2026-05-20 14:58:06 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@275c1f7a91e508645d1400d380ba5de81455a0cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-first-year-of-phd-completed",
+          title: 'First year of PhD completed!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
