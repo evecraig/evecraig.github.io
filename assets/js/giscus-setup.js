fd1560490b44f46124c9bf3f0a82d50da6332344
evecraig <ecraig@hawaii.edu>
2026-05-20 14:40:12 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@a84852834578117cecbd0a5dce3ec8325b2ebf68 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "evecraig/My-Website",
+    "data-repo": "evecraig/evecraig.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
