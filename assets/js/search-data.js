16027338e14ab3aee5b720c5268593a6aa83b0f6
evecraig <ecraig@hawaii.edu>
2026-05-21 10:51:30 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@3086fc959f3aab28b9b671f65d01cfcd50f0e752 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,17 +92,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/math/";
           
         },
-      },{id: "post-disqus-comments",
-        
-          title: "Disqus Comments",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
       },{id: "post-first-year-of-grad-school",
         
           title: "first year of grad school",
