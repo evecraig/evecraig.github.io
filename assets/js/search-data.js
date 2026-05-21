5b69982412bf6165213a4fbecad0ebe4056af1c5
evecraig <ecraig@hawaii.edu>
2026-05-21 01:34:51 +0000

    Deploying to gh-pages from @ evecraig/evecraig.github.io@51efd4682cc7d715ee132384e95d5be29e76b2ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "Planned, ongoing, and completed projects for work or done in my free time.",
           section: "Navigation",
